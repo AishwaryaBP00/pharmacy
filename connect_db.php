1d016fc46facf57dbc145ed760a6489b002a4de6
AishwaryaBP00 <aishwaryabp087@gmail.com>
2021-01-17 13:30:39 +0530

    first commit

---
+++ connect_db.php
@@ -0,0 +1,7 @@
+<?php
+
+error_reporting (1);
+$con=mysql_pconnect('den1.mysql2.gear.host','pharmacy1','Lg56~yj?Wj7K')or die("cannot connect to server");
+mysql_select_db('pharmacy1')or die("cannot connect to database");
+
+?>
